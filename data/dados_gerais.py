cab3ea0c05fe5545714ceb9160f86e5981c34ede
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2025-06-30 12:48:22 -0300

    Add DadosGerais dataclass with target channels and keywords
    Introduces the DadosGerais dataclass containing sets of target Telegram channels and relevant keywords for filtering or processing. This will be used for general data handling in the application.

---
+++ data/dados_gerais.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DadosGerais:
+    canais_alvo = {
+        "@terabyteshopoficial", 
+        "@grape_tec", 
+        "@lowpricebr", 
+        "@gpcrpromo", 
+        "@guigatudo", 
+        "@EconomizandocomJP", 
+        "@lobaopromo", 
+        "@pcdofafapromo"
+    }
+
+    palavras_chave = {
+        # "placa de vídeo",
+        # "gpu",
+        # "rtx",
+        # "geforce",
+        # "radeon",
+        # "rx ",
+        # "gtx",
+        # "arc a750",
+        # "arc a770",
+        "4060",
+        "4070",
+        "4080",
+        "4090",
+        # "7700 xt",
+        # "7800 xt",
+        # "7900 xtx",
+        "5060",
+        "5070",
+        "5080",
+        "5090",
+        "9060",
+        "9070",
+    }
